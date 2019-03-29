179fc0db55d39f3e44aa709bc1bbc43e869ed7f7
Amin <47177827+aminshoukat@users.noreply.github.com>
2019-03-29 16:47:50 +0500

    final

--- resources/views/la/employees/import.blade.php
+++ resources/views/la/employees/import.blade.php
@@ -9,24 +9,24 @@
 @section("main-content")
 
 @if ( Session::has('success') )
-        <div class="alert alert-success alert-dismissible" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">×</span>
-            <span class="sr-only">閉じる</span>
-        </button>
-        <strong>{{ Session::get('success') }}</strong>
-    </div>
-    @endif
- 
-    @if ( Session::has('error') )
-    <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">×</span>
-            <span class="sr-only">閉じる</span>
-        </button>
-        <strong>{!! Session::get('error') !!}</strong>
-    </div>
-    @endif
+    <div class="alert alert-success alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+        <span class="sr-only">閉じる</span>
+    </button>
+    <strong>{{ Session::get('success') }}</strong>
+</div>
+@endif
+
+@if ( Session::has('error') )
+<div class="alert alert-danger alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+        <span class="sr-only">閉じる</span>
+    </button>
+    <strong>{!! Session::get('error') !!}</strong>
+</div>
+@endif
  
     @if (count($errors) > 0)
     <div class="alert alert-danger">
